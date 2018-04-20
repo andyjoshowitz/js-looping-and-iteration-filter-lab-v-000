6d952b2fe3a63dbf47cf276f441c059fcd2e538f
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-04-20 15:03:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Code your solution in this file
 function findMatching (collection, name) {
-
+  const matches = collection.filter(function(driver) {return driver.toLowerCase() === name.toLowerCase()});
+  return matches;
 }
 
 function fuzzyMatch () {
